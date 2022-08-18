497f328cd2fd11e25528975faa64cd4083b1fdf5
Nicolas Filzi <nfilzi.webservices@gmail.com>
2022-08-18 18:02:27 +0200

    Add basic associations + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # GET "lists"
+  # GET "lists/42"
+  # GET "lists/new"
+  # POST "lists"
+  resources :lists, only: [:index, :show, :new, :create] do
+
+    # GET "lists/42/bookmarks/new"
+    # POST "lists/42/bookmarks"
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  # DELETE "bookmarks/25"
+  resources :bookmarks, only: [:destroy]
+
 end
